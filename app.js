dd647ae97eec104ded455350f9e8d9d210cf66e7
purv04 <puru.chaturvedi04@gmail.com>
2025-04-22 20:19:58 +0530

    port updated

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
     res.send('Hello from the Node.js app running in Docker!');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
